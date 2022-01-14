ed21ee5c0aff9c226e4cd3d00252fea0483e996d
Osebest <78011932+Osebest@users.noreply.github.com>
2022-01-14 15:11:19 +0100

    Delete gsap.js

--- gsap.js
+++
@@ -1,8 +0,0 @@
-/*GSAP ANIMATION*/
-
-var tl = gsap.timeline({defaults:{duration:1}, repeat:-1, repeatDelay:.8})
-
-tl.to(".two", {opacity:1, yoyo:true, repeat:1})
-  .to(".one", {opacity:1, yoyo:true, delay:1, repeat:1})
-  .to(".three", {opacity:1, yoyo:true, delay:1, repeat:1});
-var tll = new gsap.timeline({repeat:-1}) 
